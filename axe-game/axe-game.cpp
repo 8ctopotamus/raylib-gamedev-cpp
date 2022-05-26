a7606b5831e110d902e7ffb11888402f5fd78f1f
Josh <zylo.codes@gmail.com>
2022-05-26 18:16:57 -0500

    axe-game start

---
+++ axe-game/axe-game.cpp
@@ -0,0 +1,12 @@
+#include "raylib.h"
+
+int main(void)
+{
+    int width = 350;
+    int height = 200;
+    float root_beer = 1.99;
+    double cheese_burger{5.99};
+    bool shouldHaveLunch{};
+    InitWindow(width, height, "Josh's raylib window");
+    return 0;
+}
---
+++ vscode-template-main/hello.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main()
+{
+  printf("HELLO C++\n");
+  printf("HELLO C+++\n");
+  printf("HELLO C++++\n");
+}
