4432f84b6b48b6c088fae5f05194d803425b92b4
Josh <zylo.codes@gmail.com>
2022-05-28 11:32:35 -0500

    DrawCircle and detect keypress

--- axe-game/axe-game.cpp
+++ axe-game/axe-game.cpp
@@ -2,11 +2,39 @@
 
 int main(void)
 {
-    int width = 350;
-    int height = 200;
-    float root_beer = 1.99;
-    double cheese_burger{5.99};
-    bool shouldHaveLunch{};
-    InitWindow(width, height, "Josh's raylib window");
-    return 0;
+	int width{350};
+	int height{200};
+	InitWindow(width, height, "Josh's raylib window");
+
+	int circle_x{175};
+	int circle_y{100};
+	
+	SetTargetFPS(60);
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(WHITE);
+		
+		DrawCircle(circle_x, circle_y, 25, BLUE);
+		
+		if (IsKeyDown(KEY_D))
+		{
+			circle_x = circle_x + 10;
+		} 
+		else if (IsKeyDown(KEY_A))
+		{
+			circle_x = circle_x - 10;
+		} 
+		else if (IsKeyDown(KEY_W))
+		{
+			circle_y = circle_y - 10;
+		} 
+		else if (IsKeyDown(KEY_S))
+		{
+			circle_y = circle_y + 10;
+		} 
+		
+		EndDrawing();
+	}
+	return 0;
 }
