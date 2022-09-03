50ff1c4f31d758a3fe6ad145deee2dfa7759dce5
Josh <zylo.codes@gmail.com>
2022-09-03 16:56:56 -0500

    Adds gravity

--- dapper-dasher/dasher.cpp
+++ dapper-dasher/dasher.cpp
@@ -7,6 +7,9 @@ int main()
   InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
   SetTargetFPS(60);
 
+  // acceleraction due to gravity: (pixels/frame) / frame
+  const int gravity{1};
+
   const int width{50};
   const int height{80};
   
@@ -18,11 +21,22 @@ int main()
     BeginDrawing();
     ClearBackground(WHITE);
 
+    // perform ground check 
+    if (posY >= windowHeight - height) {
+      // rectangle is on ground
+      velocity = 0;
+    } else {
+      // apply gravity if in air
+      velocity += gravity;
+    }
+
+    // are we jumping?
     if (IsKeyPressed(KEY_SPACE)) 
     {
       velocity -= 10;
     }
 
+    // update position
     posY += velocity;
     
     DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
