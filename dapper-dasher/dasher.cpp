6d2b2083a75f55535797411db86bd84e4dabd2bf
Josh <zylo.codes@gmail.com>
2022-09-03 17:02:19 -0500

    Avoid double-jump

--- dapper-dasher/dasher.cpp
+++ dapper-dasher/dasher.cpp
@@ -1,7 +1,6 @@
 #include "raylib.h"
 
-int main() 
-{
+int main() {
   const int windowWidth{512};
   int windowHeight{380};
   InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
@@ -9,15 +8,17 @@ int main()
 
   // acceleraction due to gravity: (pixels/frame) / frame
   const int gravity{1};
+  const int jumpVelocity{-22};
 
   const int width{50};
   const int height{80};
   
+  bool isInAir = false;
+
   int posY{windowHeight - height};
   int velocity{-10};
 
-  while (!WindowShouldClose()) 
-  {
+  while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(WHITE);
 
@@ -25,15 +26,16 @@ int main()
     if (posY >= windowHeight - height) {
       // rectangle is on ground
       velocity = 0;
+      isInAir = false;
     } else {
       // apply gravity if in air
       velocity += gravity;
+      isInAir = true;
     }
 
-    // are we jumping?
-    if (IsKeyPressed(KEY_SPACE)) 
-    {
-      velocity -= 10;
+    // jump
+    if (IsKeyPressed(KEY_SPACE) && !isInAir) {
+      velocity += jumpVelocity;
     }
 
     // update position
