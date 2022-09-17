21123498e338b4bc473c21c660577619b89fcbee
Josh <zylo.codes@gmail.com>
2022-09-17 11:02:29 -0500

    Sword collision rectangle

--- classy-clash/Character.cpp
+++ classy-clash/Character.cpp
@@ -25,14 +25,37 @@ void Character::tick(float deltaTime) {
   // draw the sword
   Vector2 origin{};
   Vector2 offset{};
+  float rotation{};
   if (rightLeft > 0.f) {
     origin = {0.f, weapon.height * scale};
     offset = {40.f, 55.f};
+    rotation = 35.f;
+    weaponCollisionRec = {
+      getScreenPos().x + offset.x,
+      getScreenPos().y + offset.y - weapon.height * scale,
+      weapon.width * scale,
+      weapon.height * scale
+    };
   } else {
     origin = {weapon.width * scale, weapon.height * scale};
     offset = {25.f, 55.f};
+    rotation = -35.f;
+    weaponCollisionRec = {
+      getScreenPos().x + offset.x - weapon.width * scale,
+      getScreenPos().y + offset.y - weapon.height * scale,
+      weapon.width * scale,
+      weapon.height * scale
+    };
   }
   Rectangle source{0.f, 0.f, static_cast<float>(weapon.width) * rightLeft, static_cast<float>(weapon.height)};
   Rectangle dest{getScreenPos().x + offset.x, getScreenPos().y + offset.y, weapon.width * scale, weapon.height * scale};
-  DrawTexturePro(weapon, source, dest, origin, 0.f, WHITE);
+  DrawTexturePro(weapon, source, dest, origin, rotation, WHITE);
+  // sword collision rectangle
+  DrawRectangleLines(
+    weaponCollisionRec.x, 
+    weaponCollisionRec.y, 
+    weaponCollisionRec.width, 
+    weaponCollisionRec.height, 
+    RED
+  );
 }
