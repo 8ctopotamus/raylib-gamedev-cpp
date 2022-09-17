26942a6dc30d8c229439aed757bcabbd3de0968e
Josh <zylo.codes@gmail.com>
2022-09-17 09:20:24 -0500

    Starts refactoring Character and Enemy to inherit from BaseCharacter

--- classy-clash/BaseCharacter.h
+++ classy-clash/BaseCharacter.h
@@ -1,11 +1,32 @@
 #ifndef BASE_CHARACTER_H
 #define BASE_CHARACTER_H
+#include "raylib.h"
 
 class BaseCharacter 
 {
   public:
     BaseCharacter();
-  private:
+    Vector2 getWorldPos() { return worldPos; }
+    void undoMovement();
+    Rectangle getCollisionRec();
+  protected:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // NOTE: 1 = facing right, 2 = facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f/12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+    const float scale{4.0};
 };
 
 #endif
--- classy-clash/Character.h
+++ classy-clash/Character.h
@@ -4,26 +4,5 @@
 class Character : public BaseCharacter {
   public:
     Character(int winWidth, int winHeight);
-    Vector2 getWorldPos() { return worldPos; }
     void tick(float deltaTime);
-    void undoMovement();
-    Rectangle getCollisionRec();
-  private:
-    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    Vector2 worldPosLastFrame{};
-    // NOTE: 1 = facing right, 2 = facing left
-    float rightLeft{1.f};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    int maxFrames{6};
-    float updateTime{1.f/12.f};
-    float speed{4.f};
-    float width{};
-    float height{};
-    const float scale{4.0};
 };
